bb0e2cbce250b4c60f01af43fb624a834c527fb5
Marius Fanu <fanumarius@gmail.com>
2022-01-14 18:14:06 +0000

    Update for Swift 5

--- Round Progress View Demo/MFRoundProgressView/MFRoundProgressView.swift
+++ Round Progress View Demo/MFRoundProgressView/MFRoundProgressView.swift
@@ -3,7 +3,7 @@
 //  components
 //
 //  Created by Marius Fanu on 26/01/15.
-//  Copyright (c) 2015 Alecs Popa. All rights reserved.
+//  Copyright (c) 2015 Marius Fanu. All rights reserved.
 //
 
 import UIKit
@@ -16,6 +16,13 @@ class MFRoundProgressView: UIView {
             setNeedsDisplay()
         }
     }
+    
+    var font = UIFont.systemFont(ofSize: 22)
+    var titleColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)
+    
+    var progressColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    var progressBackgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.4)
+    
     private var startAngle = CGFloat(-90 * Double.pi / 180)
     private var endAngle = CGFloat(270 * Double.pi / 180)
     
@@ -23,16 +30,12 @@ class MFRoundProgressView: UIView {
         super.init(frame: frame)
         setup()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
     
-    private func setup() {
-        backgroundColor = .clear
-    }
-
     override func draw(_ rect: CGRect) {
         // General Declarations
         guard let context = UIGraphicsGetCurrentContext() else {
@@ -40,18 +43,21 @@ class MFRoundProgressView: UIView {
             return
         }
         
-        // Color Declarations
-        let progressColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        let progressBackgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.4)
-        let titleColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)
-        
         // Shadow Declarations
         let innerShadow = UIColor.black.withAlphaComponent(0.22)
         let innerShadowOffset = CGSize(width: 3.1, height: 3.1)
         let innerShadowBlurRadius = CGFloat(4)
         
         // Background Drawing
-        let backgroundPath = UIBezierPath(ovalIn: CGRect(x: rect.minX, y: rect.minY, width: rect.width, height: rect.height))
+        let backgroundPath = UIBezierPath(
+            ovalIn:
+                CGRect(
+                    x: rect.minX,
+                    y: rect.minY,
+                    width: rect.width,
+                    height: rect.height
+                )
+        )
         backgroundColor?.setFill()
         backgroundPath.fill()
         
@@ -64,7 +70,11 @@ class MFRoundProgressView: UIView {
         context.beginTransparencyLayer(auxiliaryInfo: nil)
         
         let opaqueShadow = innerShadow.withAlphaComponent(1)
-        context.setShadow(offset: innerShadowOffset, blur: innerShadowBlurRadius, color: opaqueShadow.cgColor)
+        context.setShadow(
+            offset: innerShadowOffset,
+            blur: innerShadowBlurRadius,
+            color: opaqueShadow.cgColor
+        )
         context.setBlendMode(CGBlendMode.sourceOut)
         context.beginTransparencyLayer(auxiliaryInfo: nil)
         
@@ -78,30 +88,57 @@ class MFRoundProgressView: UIView {
         // ProgressBackground Drawing
         let kMFPadding = CGFloat(15)
         
-        let progressBackgroundPath = UIBezierPath(ovalIn: CGRect(x: rect.minX + kMFPadding/2, y: rect.minY + kMFPadding/2, width: rect.size.width - kMFPadding, height: rect.size.height - kMFPadding))
+        let progressBackgroundPath = UIBezierPath(
+            ovalIn:
+                CGRect(
+                    x: rect.minX + kMFPadding / 2,
+                    y: rect.minY + kMFPadding / 2,
+                    width: rect.size.width - kMFPadding,
+                    height: rect.size.height - kMFPadding
+                )
+        )
         progressBackgroundColor.setStroke()
         progressBackgroundPath.lineWidth = 5
         progressBackgroundPath.stroke()
         
         // Progress Drawing
-        let progressRect = CGRect(x: rect.minX + kMFPadding/2, y: rect.minY + kMFPadding/2, width: rect.size.width - kMFPadding, height: rect.size.height - kMFPadding)
+        let progressRect = CGRect(
+            x: rect.minX + kMFPadding / 2,
+            y: rect.minY + kMFPadding / 2,
+            width: rect.size.width - kMFPadding,
+            height: rect.size.height - kMFPadding
+        )
         let progressPath = UIBezierPath()
-        progressPath.addArc(withCenter: CGPoint(x: progressRect.midX, y: progressRect.midY), radius: progressRect.width / 2, startAngle: startAngle, endAngle: (endAngle - startAngle) * (percent / 100) + startAngle, clockwise: true)
+        progressPath.addArc(
+            withCenter:
+                CGPoint(
+                    x: progressRect.midX,
+                    y: progressRect.midY),
+            radius: progressRect.width / 2,
+            startAngle: startAngle,
+            endAngle: (endAngle - startAngle) * (percent / 100) + startAngle,
+            clockwise: true
+        )
         progressColor.setStroke()
         progressPath.lineWidth = 4
         progressPath.lineCapStyle = CGLineCap.round
         progressPath.stroke()
         
         // Text Drawing
-        let textRect = CGRect(x: rect.minX, y: rect.minY, width: rect.size.width, height: rect.size.height)
+        let textRect = CGRect(
+            x: rect.minX,
+            y: rect.minY,
+            width: rect.size.width,
+            height: rect.size.height
+        )
         let textContent = NSString(string: "\(Int(percent))")
         let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
         textStyle.alignment = .center
         
         let textFontAttributes = [
-            NSAttributedStringKey.font: UIFont(name: "HelveticaNeue-Light", size: rect.width / 3)!,
-            NSAttributedStringKey.foregroundColor: titleColor,
-            NSAttributedStringKey.paragraphStyle: textStyle]
+            NSAttributedString.Key.font: font,
+            NSAttributedString.Key.foregroundColor: titleColor,
+            NSAttributedString.Key.paragraphStyle: textStyle]
         
         let textHeight = textContent.boundingRect(with: CGSize(width: textRect.width, height: textRect.height), options: .usesLineFragmentOrigin, attributes: textFontAttributes, context: nil).height
         
@@ -109,5 +146,9 @@ class MFRoundProgressView: UIView {
         context.clip(to: textRect)
         textContent.draw(in: CGRect(x: textRect.minX, y: textRect.minY + (textRect.height - textHeight) / 2, width: textRect.width, height: textHeight), withAttributes: textFontAttributes)
         context.restoreGState();
+    }
+    
+    private func setup() {
+        backgroundColor = .clear
     }
 }
--- Round Progress View Demo/ViewController.swift
+++ Round Progress View Demo/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var roundProgressView: MFRoundProgressView!
+    @IBOutlet private weak var roundProgressView: MFRoundProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func valueChanged(_ sender: UISlider) {
